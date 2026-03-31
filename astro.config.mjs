eceeec10d66d325f2dfc79e2bf8faa23b5f88ef0
Jonas <116257690+j0nes-L@users.noreply.github.com>
2026-03-31 19:20:45 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/ke-ar',
+    site: 'https://j0nes_l.github.io',
+    base: '/ke-ar',
 });
